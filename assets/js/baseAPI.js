79debe45812f549f97264b3a7e27d55cd9061c51
gxw <1641361037@qq.com>
2022-11-18 17:23:22 +0800

    登陆注册

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,5 @@
 // 每次调用ajax请求之前先调用这个函数，拿到我们给ajax提供的配置对象
-const path = require('path');
-$.ajacPrefilter(function(options) {
-    let url = parent.join('http://www.liulongbin.top:3007', options.url);
-    console.log(url);
+$.ajaxPrefilter(function(options) {
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+    console.log(options.url);
 })
